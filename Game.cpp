67664cc64aa6b432e1fd37fd983a71ded24ad66f
mserezlija <msserezlija@gmail.com>
2026-01-19 04:37:52 +0100

    HumanPlayer can pick color

--- Game.cpp
+++ Game.cpp
@@ -6,24 +6,67 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 Game::Game(int human_players) :curr_player(0), num_human(human_players) {
 	srand(time(0));
 
+	bool taken_colors[GameConstants::NUM_PLAYERS] = { false };
+
 	for (int i = 0; i < GameConstants::NUM_PLAYERS; i++) {
 		string name = (i < num_human) ? "igrac " + to_string(i + 1) : "komp" + to_string(i + 1);
 
+		string color;
+		int start_pos = GameConstants::START_POSITIONS[i];
+
+		if (i < num_human) { color = pick_color(i+1,taken_colors); }
+		else {
+			for (int i = 0; i < GameConstants::NUM_PLAYERS; i++) {
+				if (!taken_colors[i]) {
+					taken_colors[i] = true;
+					color = GameConstants::PLAYER_COLORS[i];
+					break;
+				}
+			}
+		}
+
 		if (i < num_human) {
-			players[i] = new HumanPlayer(name, GameConstants::PLAYER_COLORS[i], GameConstants::START_POSITIONS[i]);
+			players[i] = new HumanPlayer(name, color, start_pos);
 		}
 		else {
-			players[i] = new ComputerPlayer(name, GameConstants::PLAYER_COLORS[i], GameConstants::START_POSITIONS[i]);
+			players[i] = new ComputerPlayer(name, color, start_pos);
 		}
 	}
 }
 
+string Game::pick_color(int player_n, bool taken_colors[]) {
+	cout << "odaberi boju: ";
+	for (int i = 0; i < GameConstants::NUM_PLAYERS; i++) {
+		if (!taken_colors[i]) {
+			cout << "  " << (i + 1) << " - " << GameConstants::PLAYER_COLORS[i] << endl;
+		}
+	}
+
+	int picked;
+	cin >> picked;
+	cin.ignore();
+	picked--;
+
+	if (picked >= 0 && picked < GameConstants::NUM_PLAYERS && !taken_colors[picked]) {
+		taken_colors[picked] = true;
+		return GameConstants::PLAYER_COLORS[picked];
+	}
+	cout << "neispravan unos, automatski dodiljenja boja" << endl;
+	for (int i = 0; i< GameConstants::NUM_PLAYERS; i++) {
+		if (!taken_colors[i]) {
+			taken_colors[i] = true;
+			return GameConstants::PLAYER_COLORS[i];
+		}
+	}
+}
 
 Game::~Game() {
 	for (int i = 0; i < GameConstants::NUM_PLAYERS; i++) {
--- Game.h
+++ Game.h
@@ -3,12 +3,16 @@
 
 #include "Player.h"
 #include "GameConstants.h"
+#include <vector>
+#include <string>
 
 class Game {
 private:
 	Player* players[GameConstants::NUM_PLAYERS];
 	int curr_player;
 	int num_human;
+
+	string pick_color(int player_n, bool taken_colors[]);
 
 	void eat(Player* current_player, Piece* moved_piece);
 public:
